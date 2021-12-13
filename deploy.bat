8c720f7129aaf10b9602336aa7ad2090855bcdad
jpabloroa <jpablonano@gmail.com>
2021-12-13 00:27:55 -0500

    Create deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,25 @@
+@echo off
+
+D:
+cd "\OneDrive\Escritorio\CLASE JAVA\JAVASCRIPT\php\PROYECTOS\ROVERIN"
+
+git add src/
+
+git commit -am"Corrección de errores"
+
+git push
+
+echo ***********************************************************
+echo ******************** Mixing branches... *******************
+echo ***********************************************************
+
+C:
+cd "\xampp\htdocs\proyectos\roverin"
+
+git pull
+
+echo ***********************************************************
+echo *********************** Updated ! *************************
+echo ***********************************************************
+
+rem PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Aplicacion actualizada correctamente!')"
