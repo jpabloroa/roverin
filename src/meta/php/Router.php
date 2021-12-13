c9fda494199621d60b4099e3e016da6ab581731f
jpabloroa <jpablonano@gmail.com>
2021-12-13 12:26:42 -0500

    Correcci├│n de errores

--- src/meta/php/Router.php
+++ src/meta/php/Router.php
@@ -24,6 +24,7 @@ class Route
         $inputPath = (isset($_SERVER["PATH_INFO"])) ? $_SERVER["PATH_INFO"] : "/";
         foreach (self::$routes as $path => $content) {
 
+            echo $path;
             if ($path == $inputPath) {
 
                 //
