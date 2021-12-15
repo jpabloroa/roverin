64bd4d1897a5683112141629858cd5ab3e9dbd49
jpabloroa <jpablonano@gmail.com>
2021-12-15 12:57:48 -0500

    Correcci├│n de errores

--- src/pages/html/home/home.php
+++ src/pages/html/home/home.php
@@ -45,9 +45,9 @@ Roverin Technologics - 2021
 -->
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" type="text/css" href="src/pages/html/home/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php toBase64("text/css", "assets/css/bootstrap.min.css"); ?>">
 
-    <link rel="stylesheet" type="text/css" href="src/pages/html/home/assets/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="<?php toBase64("text/css", "assets/css/font-awesome.css"); ?>">
 
     <link rel="stylesheet" type="text/css" href="<?php toBase64("text/css", "assets/css/style.css"); ?>">
 
@@ -501,20 +501,20 @@ Roverin Technologics - 2021
     </footer>
 
     <!-- jQuery -->
-    <script src="src/pages/html/home/assets/js/jquery-2.1.0.min.js"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/jquery-2.1.0.min.js"); ?>"></script>
 
     <!-- Bootstrap -->
-    <script src="src/pages/html/home/assets/js/popper.js"></script>
-    <script src="src/pages/html/home/assets/js/bootstrap.min.js"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/jquery-2.1.0.min.js"); ?>src/pages/html/home/assets/js/popper.js"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/bootstrap.min.js"); ?>"></script>
 
     <!-- Plugins -->
-    <script src="src/pages/html/home/assets/js/scrollreveal.min.js"></script>
-    <script src="src/pages/html/home/assets/js/waypoints.min.js"></script>
-    <script src="src/pages/html/home/assets/js/jquery.counterup.min.js"></script>
-    <script src="src/pages/html/home/assets/js/imgfix.min.js"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/scrollreveal.min.js"); ?>"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/waypoints.min.js"); ?>"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/jquery.counterup.min.js"); ?>"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/imgfix.min.js"); ?>src/pages/html/home/assets/js/imgfix.min.js"></script>
 
     <!-- Global Init -->
-    <script src="src/pages/html/home/assets/js/custom.js"></script>
+    <script src="<?php toBase64("text/javascript", "assets/js/custom.js"); ?>"></script>
 
 </body>
 
