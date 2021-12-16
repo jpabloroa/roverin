062f2934e06fade00941f2f729f5803e1d14a01f
jpabloroa <jpablonano@gmail.com>
2021-12-16 13:40:53 -0500

    Correcci├│n de errores

--- src/mvc/controlador/ServerController.php
+++ src/mvc/controlador/ServerController.php
@@ -103,8 +103,7 @@ class ServerController extends BaseController
         $isClosed = session_destroy();
         unset($_SERVER["PHP_AUTH_USER"]);
         unset($_SERVER["PHP_AUTH_PW"]);
-        $this->sendOutput(204, [], ["No Content"], "Funcion close llamada, se cierra la sesion?: $isClosed<br>Usuario: " . $_SERVER["PHP_AUTH_USER"]);
-        exit;
+        $this->redirectToIndex();
     }
 
     public function showVariables()
