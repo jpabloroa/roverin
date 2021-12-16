d271fb1fb6a807a1c69fa4dbf82747975d3fb5e6
jpabloroa <jpablonano@gmail.com>
2021-12-15 23:29:40 -0500

    Correcci├│n de errores

--- src/mvc/vista/administrador.php
+++ src/mvc/vista/administrador.php
@@ -4,7 +4,28 @@
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
-    echo 'Text to send if user hits Cancel button';
+    $_REQUEST["error"] = "404";
+    //include(__DIR__ . "/../../" . self::$errorRoutes["404"]);
+
+    //
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+        $link = "https";
+    } else {
+        $link = "http";
+    }
+
+    // Here append the common URL characters.
+    $link .= "://";
+
+    // Append the host(domain name, ip) to the URL.
+    $link .= $_SERVER['HTTP_HOST'];
+
+    // Append the requested resource location to the URL 
+    $link .= $_SERVER['REQUEST_URI'];
+    echo "Redireccionando a $link";
+
+    //
+    //header("location: $link");
     exit;
 } else {
     echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
