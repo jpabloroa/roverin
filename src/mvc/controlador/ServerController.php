64f9bf745162e63b13f315397d13e277d74ece59
jpabloroa <jpablonano@gmail.com>
2021-12-16 13:05:56 -0500

    Correcci├│n de errores

--- src/mvc/controlador/ServerController.php
+++ src/mvc/controlador/ServerController.php
@@ -71,12 +71,12 @@ class ServerController extends BaseController
 
                     if ($this->requestPath[1] == "http") {
 
-                        if (count($this->requestPath) > 3 && $this->requestPath[2] != "") {
+                        if (count($this->requestPath) >= 3 && $this->requestPath[2] != "") {
                             $this->manageHttp(array_diff($this->requestPath, $this->requestPath[1]));
                         } else {
                             $this->manageHttp();
                         }
-                    } else if (count($this->requestPath) > 3 && $this->requestPath[2] != "") {
+                    } else if (count($this->requestPath) >= 3 && $this->requestPath[2] != "") {
                         $this->{$this->requestPath[1]}(array_diff($this->requestPath, $this->requestPath[1]));
                     } else {
                         $this->sendOutput(403, [], ["Bad Request"], "Error en la solicitud: " . implode("/", $this->requestPath));
