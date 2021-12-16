5d4cfc9167af375990e122e8e0557640a4c72e79
jpabloroa <jpablonano@gmail.com>
2021-12-15 23:23:52 -0500

    Correcci├│n de errores

--- src/mvc/vista/administrador.php
+++ src/mvc/vista/administrador.php
@@ -1,6 +1,12 @@
 <?php
-session_start();
-if (empty($_SESSION['logged_in'])) {
-    echo "viva el perico";
+
+//
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
     exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
 }
