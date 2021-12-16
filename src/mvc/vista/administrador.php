881e0b898820f5387f23d3d69a2ad0dae124fe8e
jpabloroa <jpablonano@gmail.com>
2021-12-15 23:59:34 -0500

    Correcci├│n de errores

--- src/mvc/vista/administrador.php
+++ src/mvc/vista/administrador.php
@@ -1,25 +1,37 @@
 <?php
 
-/*
+
 foreach($_SERVER as $column => $value){
     echo "$column => $value <br>";
 }
-*/
 
 
+/*
 //
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
 
     //This excecutes if theres not a succesful login
-    $_SERVER["PATH_INFO"] = "/";
-    include(__DIR__ . "/../../router/Routes.php");
-    header_remove('WWW-Authenticate: Basic realm="My Realm"');
-    header_remove('HTTP/1.0 401 Unauthorized'); 
-    header("HTTP/1.0 200"); 
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+        $link = "https";
+    } else {
+        $link = "http";
+    }
+
+    // Here append the common URL characters.
+    $link .= "://";
+
+    // Append the host(domain name, ip) to the URL.
+    $link .= $_SERVER['HTTP_HOST'];
+
+    // Append the requested resource location to the URL 
+    $link .= $_SERVER['REQUEST_URI'];
+    echo "Redireccionando a $link";
+    echo '<script>window.location.replace("' . $link . '");</script>';
     exit;
 } else {
     echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
     echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
 }
+*/
