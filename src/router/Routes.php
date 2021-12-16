301e41c7d477647a4bebc070f4e488d106caddbe
jpabloroa <jpablonano@gmail.com>
2021-12-16 14:40:25 -0500

    Correcci├│n de errores

--- src/router/Routes.php
+++ src/router/Routes.php
@@ -5,8 +5,6 @@ require_once __DIR__ . "/../meta/php/Router.php";
 Route::add("/", "/pages/html/home/home.php");
 Route::add("/&access", "/mvc/vista/administrador.php");
 Route::add("/peri", "/mvc/vista/inicio.php");
-Route::add("/pai", "/mvc/vista/inicio.php");
-Route::add("/route", "/mvc/vista/inicio.php");
 
 Route::addError("404", "/pages/error/error.php");
 
