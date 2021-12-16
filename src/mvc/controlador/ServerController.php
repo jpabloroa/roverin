b8d570fd968a817c74ebc0651d352ef5aba126b9
jpabloroa <jpablonano@gmail.com>
2021-12-16 11:26:53 -0500

    Correcci├│n de errores

--- src/mvc/controlador/ServerController.php
+++ src/mvc/controlador/ServerController.php
@@ -13,74 +13,78 @@ class ServerController extends BaseController
 
     public function __construct()
     {
-        $this->httpMethod = strtoupper($_SERVER["REQUEST_METHOD"]);
-        $this->requestPath = (isset($_SERVER["PATH_INFO"])) ? explode("/", str_replace("/&access", "/", $_SERVER["PATH_INFO"])) : ["/", ""];;
+        try {
+            $this->httpMethod = strtoupper($_SERVER["REQUEST_METHOD"]);
+            $this->requestPath = (isset($_SERVER["PATH_INFO"])) ? explode("/", str_replace("/&access", "/", $_SERVER["PATH_INFO"])) : ["/", ""];;
 
-        //
-        session_start();
+            //
+            session_start();
 
-        //
-        if (!isset($_SESSION["AUTH_USER"]) || $_SESSION["AUTH_USER"] == "" || !isset($_SESSION["AUTH_PW"]) || $_SESSION["AUTH_PW"] == "") {
+            //
+            if (!isset($_SESSION["AUTH_USER"]) || $_SESSION["AUTH_USER"] == "" || !isset($_SESSION["AUTH_PW"]) || $_SESSION["AUTH_PW"] == "") {
 
-            if (isset($_GET["login"])) {
-                $credentials = explode(":", $_GET["login"]);
-                if (isset($credentials[0]) && isset($credentials[1])) {
-                    $_SESSION["PHP_AUTH_USER"] = $credentials[0];
-                    $_SESSION["PHP_AUTH_PW"] = $credentials[1];
+                if (isset($_GET["login"])) {
+                    $credentials = explode(":", $_GET["login"]);
+                    if (isset($credentials[0]) && isset($credentials[1])) {
+                        $_SESSION["PHP_AUTH_USER"] = $credentials[0];
+                        $_SESSION["PHP_AUTH_PW"] = $credentials[1];
+                    }
                 }
-            }
 
-            if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] == "") {
-                header('WWW-Authenticate: Basic realm="Inicie sesión para continuar"');
-                header('HTTP/1.0 401 Unauthorized');
+                if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] == "") {
+                    header('WWW-Authenticate: Basic realm="Inicie sesión para continuar"');
+                    header('HTTP/1.0 401 Unauthorized');
 
-                //This excecutes if theres not a succesful login
-                //$this->redirectToIndex();
-                echo "no se ha autenticado";
+                    //This excecutes if theres not a succesful login
+                    //$this->redirectToIndex();
+                    echo "no se ha autenticado";
+                } else {
+
+                    require_once __DIR__ . "/../modelo/ServerModel.php";
+
+                    $serverController = new ServerModel();
+                    if ($serverController->validateUser(
+                        $this->bindParams(["'", "=", "/", "\\"], $_SERVER["PHP_AUTH_USER"]),
+                        $this->bindParams(["'", "=", "/", "\\"], $_SERVER["PHP_AUTH_PW"])
+                    )) {
+                        $_SESSION["AUTH_USER"] = $_SERVER["PHP_AUTH_USER"];
+                        $_SESSION["AUTH_PW"] = $_SERVER["PHP_AUTH_PW"];
+
+                        $this->sendOutput(202, [], ["Accepted"], "Bienvenido " . $this->userName);
+                    } else {
+                        $_SESSION["AUTH_USER"] = $_SERVER["PHP_AUTH_USER"];
+                        $_SESSION["AUTH_PW"] = $_SERVER["PHP_AUTH_PW"];
+                        echo "usuario no valido, credenciales: " . $_SERVER["PHP_AUTH_USER"] . " y " . $_SERVER["PHP_AUTH_PW"];
+                        //$this->redirectToIndex();
+                        //header('HTTP/1.0 401 Unauthorized');
+                        //header('Sin autorizar');
+                    }
+                }
             } else {
 
-                require_once __DIR__ . "/../modelo/ServerModel.php";
+                $this->userName = $_SESSION["AUTH_USER"];
+                $this->passWord = $_SESSION["AUTH_PW"];
 
-                $serverController = new ServerModel();
-                if ($serverController->validateUser(
-                    $this->bindParams(["'", "=", "/", "\\"], $_SERVER["PHP_AUTH_USER"]),
-                    $this->bindParams(["'", "=", "/", "\\"], $_SERVER["PHP_AUTH_PW"])
-                )) {
-                    $_SESSION["AUTH_USER"] = $_SERVER["PHP_AUTH_USER"];
-                    $_SESSION["AUTH_PW"] = $_SERVER["PHP_AUTH_PW"];
+                if (isset($this->requestPath[1]) && $this->requestPath[1] != "") {
 
-                    $this->sendOutput(202, [], ["Accepted"], "Bienvenido " . $this->userName);
-                } else {
-                    $_SESSION["AUTH_USER"] = $_SERVER["PHP_AUTH_USER"];
-                    $_SESSION["AUTH_PW"] = $_SERVER["PHP_AUTH_PW"];
-                    echo "usuario no valido, credenciales: " . $_SERVER["PHP_AUTH_USER"] . " y " . $_SERVER["PHP_AUTH_PW"];
-                    //$this->redirectToIndex();
-                    //header('HTTP/1.0 401 Unauthorized');
-                    //header('Sin autorizar');
-                }
-            }
-        } else {
+                    if ($this->requestPath[1] == "http") {
 
-            $this->userName = $_SESSION["AUTH_USER"];
-            $this->passWord = $_SESSION["AUTH_PW"];
-
-            if (isset($this->requestPath[1]) && $this->requestPath[1] != "") {
-
-                if ($this->requestPath[1] == "http") {
-
-                    if (isset($this->requestPath[2]) && $this->requestPath[2] != "") {
-                        $this->manageHttp($this->requestPath[2]);
+                        if (isset($this->requestPath[2]) && $this->requestPath[2] != "") {
+                            $this->manageHttp($this->requestPath[2]);
+                        } else {
+                            $this->manageHttp();
+                        }
                     } else {
-                        $this->manageHttp();
-                    }
-                } else {
-                    if (isset($this->requestPath[2]) && $this->requestPath[2] != "") {
-                        $this->{$this->requestPath[1]}($this->requestPath[2]);
-                    } else {
-                        $this->{($this->requestPath[1] == "") ? "defaultMethod" : $this->requestPath[1]}();
+                        if (isset($this->requestPath[2]) && $this->requestPath[2] != "") {
+                            $this->{$this->requestPath[1]}($this->requestPath[2]);
+                        } else {
+                            $this->{($this->requestPath[1] == "") ? "defaultMethod" : $this->requestPath[1]}();
+                        }
                     }
                 }
             }
+        } catch (Exception $e) {
+            $this->sendOutput(500, [], ["Internal Server Error"], "Error interno del servidor<br>Detalles: " . $e->getMessage());
         }
     }
 
