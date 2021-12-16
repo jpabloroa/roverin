9d5cca5da59d8d3f970161a1503a4f2618e128eb
jpabloroa <jpablonano@gmail.com>
2021-12-15 23:46:15 -0500

    Correcci├│n de errores

--- src/mvc/vista/administrador.php
+++ src/mvc/vista/administrador.php
@@ -11,29 +11,9 @@ foreach($_SERVER as $column => $value){
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
-    $_REQUEST["error"] = "404";
 
-    //
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-        $link = "https";
-    } else {
-        $link = "http";
-    }
-
-    // Here append the common URL characters.
-    $link .= "://";
-
-    // Append the host(domain name, ip) to the URL.
-    $link .= $_SERVER['HTTP_HOST'];
-
-    // Append the requested resource location to the URL 
-    $rqst_uri = $_SERVER['SCRIPT_NAME'];
-    $link .= substr($rqst_uri, "0", strlen($rqst_uri) - 21);
-    //echo "Redireccionando a $link";
-    include($rqst_uri);
-
-    //
-    //header("location: $link");
+    //This excecutes if theres not a succesful login
+    include(__DIR__ . "../../router/Routes.php");
     exit;
 } else {
     echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
