f5517f54cb0e4fe5ba7ec5c3d93cc996d09c1291
jpabloroa <jpablonano@gmail.com>
2021-12-15 23:14:01 -0500

    Correcci├│n de errores

--- src/mvc/vista/administrador.php
+++ src/mvc/vista/administrador.php
@@ -1,2 +1,12 @@
 <?php
-echo $_SERVER["PATH_INFO"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from Form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username == "juan" && $password == "loco") {
+        echo "Sapagonorrea";
+    } else {
+        header("Refresh:0");
+    }
+}
