50fc2d9e1e0147105842e5cd1a2914dd85199f1c
jpabloroa <jpablonano@gmail.com>
2021-12-17 13:32:19 -0500

    Correcci├│n de errores

--- src/mvc/vista/formulario_home.php
+++ src/mvc/vista/formulario_home.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once __DIR__ . "/../controlador/UserController.php";
+try {
+    require_once __DIR__ . "/../controlador/UserController.php";
 
-$userController = new UserController();
-$userController->formHomePage();
+    $userController = new UserController();
+    $userController->formHomePage();
+} catch (Exception $e) {
+    echo "Error!: " . $e->getMessage();
+}
