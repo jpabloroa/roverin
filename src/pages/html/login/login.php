9bbb7544aefc49a9754b38ef5a34ab43838ed499
jpabloroa <jpablonano@gmail.com>
2021-12-17 21:01:43 -0500

    Correcci├│n de errores

--- src/pages/html/login/login.php
+++ src/pages/html/login/login.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de sesión</title>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 
 
 </head>
@@ -19,9 +20,16 @@
     <input type="text">
 
     <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v12.0&appId=331230838551755&autoLogAppEvents=1" nonce="GRaZWCk9"></script>
     <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
     <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId: '331230838551755',
+                autoLogAppEvents: true,
+                xfbml: true,
+                version: 'v12.0'
+            });
+        };
         FB.api('/me', {
             fields: 'name'
         }, function(response) {
