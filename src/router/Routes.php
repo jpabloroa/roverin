1502dc47510aab99e2ab95a57c891036f01d9d36
jpabloroa <jpablonano@gmail.com>
2021-12-21 13:33:15 -0500

    Correcci├│n de errores

--- src/router/Routes.php
+++ src/router/Routes.php
@@ -6,7 +6,7 @@ Route::add("/", "/pages/html/home/home.php");
 //Route::add("/", "/mvc/vista/inicio.php");
 Route::add("/login", "/pages/html/login/login.php");
 Route::add("/&access", "/mvc/vista/administrador.php");
-Route::add("/canaldigital", "/CRUD-CANAL-CUSEZAR-PHP/index.html");
+Route::add("/canaldigital", "/../CRUD-CANAL-CUSEZAR-PHP/index.html");
 
 Route::addError("404", "/pages/error/error.php");
 
